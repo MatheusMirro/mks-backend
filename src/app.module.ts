b6270889879c213d8a43852dbbb3918854c53c8b
Matheus Alves <mirro123@outlook.com>
2023-11-27 15:27:58 -0300

    Configured AppModule to connect to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
-
+import { AuthService } from './auth/auth.service';
+import { JwtStrategy } from './auth/jwt.strategy';
+import { AuthModule } from './auth/auth.module';
+import { JwtService } from '@nestjs/jwt';
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      // Configurações de conexão com o banco de dados
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'mks-movies-database',
+      entities: [User],
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forFeature([User]),
+  ],
+
+  providers: [AuthService, JwtService, JwtStrategy],
 })
 export class AppModule {}
