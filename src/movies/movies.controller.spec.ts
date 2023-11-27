123c69c0d4f496bf300e418ee2bd689eae1a2960
Matheus Alves <mirro123@outlook.com>
2023-11-27 16:21:17 -0300

    Added tests for MoviesController and MoviesService

--- src/movies/movies.controller.spec.ts
+++ src/movies/movies.controller.spec.ts
@@ -3,18 +3,18 @@ import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 
 describe('MoviesController', () => {
-  let controller: MoviesController;
+  let moviesController: MoviesController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
       controllers: [MoviesController],
       providers: [MoviesService],
     }).compile();
 
-    controller = module.get<MoviesController>(MoviesController);
+    moviesController = moduleRef.get<MoviesController>(MoviesController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(moviesController).toBeDefined();
   });
 });
