023b2b6ab2464533c43a4c5792efe70fd68c02f2
Matheus Alves <mirro123@outlook.com>
2023-11-27 15:33:25 -0300

    Configured ValidationPipe and Swagger in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação com Swagger - Crud Mks Backend-Challenge')
+    .setDescription(
+      'Aplicação onde um usuário com token válido pode inserir filmes, deletar, checar quantos filmes tem e editar filmes',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(5000);
 }
 bootstrap();
